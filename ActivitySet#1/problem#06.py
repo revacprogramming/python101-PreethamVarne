72b02f2da34f8800b02cb7fb623258ca94d0a4fd
Preetham Varne <97089332+PreethamVarne@users.noreply.github.com>
2022-05-13 03:22:39 +0000

    Done

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -1,16 +1,17 @@
-# Loops & Iterators
+# Functions
 
-largest = None
-smallest = None
+def computepay(h, r):
+    if h > 40:
+        p = 1.5 * r * (h - 40) + (40 *r)
+    else:
+    	p = h * r
+    return p
+    
+    
+hrs = input("Enter Hours:")
+h = int(hrs)
+rate = input("rate:")
+r = float(rate)
 
-while True:
-    num = input("Enter a number? ")
-
-    if num == "done":
-        break
-
-    # ...
-
-    print(num)
-
-print("Maximum", largest)
+p = computepay(h, r)
+print("Pay", p)
