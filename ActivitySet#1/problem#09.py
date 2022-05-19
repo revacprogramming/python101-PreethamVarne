4eb9ab44a7ab9e930e4dc0ee9d938e06437b1d97
Preetham Varne <97089332+PreethamVarne@users.noreply.github.com>
2022-05-19 14:29:50 +0000

    Done

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,10 @@
-# Lists
-
-filename = "dataset/romeo.txt"
+name = input("Enter the file name: ","r")
+fhand=open(name)
+sm=0
+count=0
+fread=fhand.readlines()
+for line in fread:
+    if line.startswith("X-DSPAM-Confidence:"):
+        sm+=float(line.split()[1])
+        count+=1
+print(f"Average spam confidence: {sm/count}")
