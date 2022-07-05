d124f1661df175b1afd772b8ee56eb9cd3d00ed0
Preetham Varne <97089332+PreethamVarne@users.noreply.github.com>
2022-07-05 09:19:52 +0000

    Done

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,17 @@
-# Regular.  Ex pressions
-# https://www.py4e.com/lessons/regex
+name = input("Enter file:")
+handle = open(name)
+lst = list()
+for line in handle:
+    if not line.startswith("From:"):continue
+    line = line.split()
+    lst.append(line[1])
+counts = dict()
+for word in lst:
+	counts[word] = counts.get(word,0) + 1
+bigcount = None
+bigword = None
+for word,count in counts.items():
+    if bigcount is None or count > bigcount:
+        bigcount = count
+        bigword = word
+print(bigword , bigcount)
